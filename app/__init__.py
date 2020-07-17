b31d8afff77a4e0bd2c718c0b994e4a918d91560
awesome94 <suubidavid@gmail.com>
2020-07-17 11:43:24 +0300

    Add access to routes

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from os import environ
 from dotenv import load_dotenv, find_dotenv
+from flask_cors import CORS
 from flask_migrate import Migrate, MigrateCommand
 from config import config
 
@@ -14,6 +15,7 @@ login = LoginManager()
 
 def create_app(config_name):
     app = Flask(__name__)
+    CORS(app)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
